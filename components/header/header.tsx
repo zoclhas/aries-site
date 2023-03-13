4672c2de5cdd3f3a05e30fa3946ca0164381edc6
Zoclhas <zoclhas@gmail.com>
2023-03-13 19:48:47 +0400

    Navbar done

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,1 +1,118 @@
-export function Header() {}
+import {
+    createStyles,
+    Menu,
+    Center,
+    Header,
+    Container,
+    Group,
+    Button,
+    Burger,
+    rem,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import Link from "next/link";
+// import { IconChevronDown } from "@tabler/icons-react";
+import { AriesLogo } from "@/components/icons";
+
+const HEADER_HEIGHT = rem(60);
+
+const useStyles = createStyles((theme) => ({
+    header: {
+        position: "fixed",
+        boxShadow: "0px 4px 16px 5px rgb(0, 0, 0, 0.15)",
+    },
+
+    inner: {
+        height: HEADER_HEIGHT,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        paddingLeft: rem(50),
+        paddingRight: rem(50),
+
+        [theme.fn.smallerThan("sm")]: {
+            paddingLeft: rem(20),
+            paddingRight: rem(20),
+        },
+    },
+
+    links: {
+        [theme.fn.smallerThan("sm")]: {
+            display: "none",
+        },
+    },
+
+    burger: {
+        [theme.fn.largerThan("sm")]: {
+            display: "none",
+        },
+    },
+
+    link: {
+        display: "block",
+        lineHeight: 1,
+        padding: `${rem(8)} ${rem(12)}`,
+        borderRadius: theme.radius.md,
+        textDecoration: "none",
+        color:
+            theme.colorScheme === "dark"
+                ? theme.colors.dark[0]
+                : theme.colors.gray[7],
+        fontSize: theme.fontSizes.sm,
+        fontWeight: 500,
+
+        transition: "175ms ease-in-out",
+
+        "&:hover": {
+            backgroundColor:
+                theme.colorScheme === "dark"
+                    ? theme.colors.dark[6]
+                    : theme.colors.gray[0],
+        },
+    },
+
+    linkLabel: {
+        marginRight: rem(5),
+    },
+}));
+
+interface HeaderActionProps {
+    links: {
+        link: string;
+        label: string;
+    }[];
+}
+
+export function Navbar({ links }: HeaderActionProps) {
+    const { classes } = useStyles();
+    const [opened, { toggle }] = useDisclosure(false);
+    const items = links.map((link) => {
+        return (
+            <Link key={link.label} href={link.link} className={classes.link}>
+                {link.label}
+            </Link>
+        );
+    });
+
+    return (
+        <Header
+            height={HEADER_HEIGHT}
+            sx={{ borderBottom: 0 }}
+            className={classes.header}
+        >
+            <Container className={classes.inner} fluid>
+                <Group>
+                    <Link href="/">
+                        <AriesLogo height={30} />
+                    </Link>
+                </Group>
+                <Group spacing={5} className={classes.links}>
+                    {items}
+                </Group>
+                <Button component="a" radius="md" variant="light" h={30}>
+                    Add to server
+                </Button>
+            </Container>
+        </Header>
+    );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import { AppProps } from "next/app";
 // import Head from "next/head";
 import { MantineProvider } from "@mantine/core";
+import "@/styles/main.css";
+
+import { Navbar } from "@/components/header/header";
 
 export default function App(props: AppProps) {
     const { Component, pageProps } = props;
+
+    const links = [
+        { link: "/", label: "Home" },
+        { link: "/docs", label: "Docs" },
+        { link: "/faq", label: "FaQ" },
+    ];
 
     return (
         <>
@@ -12,8 +21,24 @@ export default function App(props: AppProps) {
                 withNormalizeCSS
                 theme={{
                     colorScheme: "dark",
+                    colors: {
+                        brand: [
+                            "#F0BBDD",
+                            "#ED9BCF",
+                            "#EC7CC3",
+                            "#ED5DB8",
+                            "#F13EAF",
+                            "#F71FA7",
+                            "#FF00A1",
+                            "#E00890",
+                            "#C50E82",
+                            "#AD1374",
+                        ],
+                    },
+                    primaryColor: "brand",
                 }}
             >
+                <Navbar links={links} />
                 <Component {...pageProps} />
             </MantineProvider>
         </>
